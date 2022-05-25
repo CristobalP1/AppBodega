206c8d49fd864431484c40d7e8aee719220effc4
cristobal-1 <cristobal1@gmail.com>
2022-05-25 19:32:12 -0400

    adding widget input from create-product-screen

--- lib/screens/create_product_screen.dart
+++ lib/screens/create_product_screen.dart
@@ -7,14 +7,48 @@ class CreateProductScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Productos'),
+        backgroundColor: Colors.pinkAccent,
+        leading: const Icon(Icons.menu_outlined),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.attach_file_rounded),
+            onPressed: () {
+              print('holaa');
+            },
+          )
+        ],
+      ),
       body: Center(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: const [
-            InputForm(labelText: 'Nombre Producto', hintText: 'leche'),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              'Crear producto',
+              style: TextStyle(fontSize: 20),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            InputForm(
+              labelText: "nombre",
+              hintText: "leche",
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            InputForm(
+              labelText: "nombre",
+              hintText: "leche",
+            ),
           ],
         ),
       ),
-
     );
   }
 }
