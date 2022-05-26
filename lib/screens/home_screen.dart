e1fe789da4fac5d8f66453acf0bf4b08a6a2c56a
gitKoibumi <diego.tapia.968@gmail.com>
2022-05-25 20:32:04 -0400

    add buton low

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -10,42 +11,58 @@ class HomeScreen extends StatelessWidget {
     TextStyle fontSize23 = const TextStyle(fontSize: 23);
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Productos'),
-        backgroundColor: Colors.blueAccent,
-        leading: const Icon(Icons.menu_outlined),
-        elevation: 0,
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.attach_file_rounded),
-            onPressed: () {
-              print('holaa');
-            },
-          )
-        ],
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Contadorrr',
-              style: fontSize23,
-            ),
-            Text(
-              '0',
-              style: fontSize23,
-            ),
+        appBar: AppBar(
+          title: Text('Productos'),
+          backgroundColor: Color.fromARGB(255, 236, 55, 137),
+          leading: const Icon(Icons.menu_outlined),
+          elevation: 0,
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.attach_file_rounded),
+              onPressed: () {
+                print('holaa');
+              },
+            )
           ],
         ),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.miniEndFloat,
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.add),
-        onPressed: () {
-          print('hola mundo');
-        },
-      ),
-    );
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'Contadorrr',
+                style: fontSize23,
+              ),
+              Text(
+                '0',
+                style: fontSize23,
+              ),
+            ],
+          ),
+        ),
+        floatingActionButton: SpeedDial(
+          animatedIcon: AnimatedIcons.menu_close,
+          backgroundColor: Color.fromARGB(255, 236, 55, 137),
+          overlayColor: Colors.black,
+          overlayOpacity: 0.5,
+          children: [
+            SpeedDialChild(
+                child: Icon(Icons.create),
+                label: 'Crear Producto',
+                backgroundColor: Color.fromARGB(255, 118, 170, 120)),
+            SpeedDialChild(
+                child: Icon(Icons.create),
+                label: 'Crear Categoria',
+                backgroundColor: Color.fromARGB(255, 118, 170, 120)),
+            SpeedDialChild(
+                child: Icon(Icons.block),
+                label: 'Bloquear Prodcuto',
+                backgroundColor: Color.fromARGB(255, 212, 59, 69)),
+            SpeedDialChild(
+                child: Icon(Icons.block),
+                label: 'Bloquear Categoria',
+                backgroundColor: Color.fromARGB(255, 212, 59, 69))
+          ],
+        ));
   }
 }
