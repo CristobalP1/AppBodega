8082a1eb6275660d3082ea6aec8f0752dcc27064
felix <felix.vr515@gmail.com>
2022-05-27 23:39:12 -0400

    add code

--- lib/screens/PurchaseOrders/create_order_screen.dart
+++ lib/screens/PurchaseOrders/create_order_screen.dart
@@ -1,14 +1,102 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/widget/button_save_canceller.dart';
 
 class CreateOrderScreen extends StatelessWidget {
   const CreateOrderScreen({Key? key}) : super(key: key);
-
+static const String titulo = 'Crear Pedido';
+  
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('CreateOrderScreen'),
+
+    return Scaffold(    
+        
+      appBar: AppBar(
+        title: const Text(titulo),
+        leading: const Icon(Icons.menu_outlined),
+        elevation: 15.5, 
+        ),
+
+      body: Column(
+        children:  [
+          Container(
+            padding: const EdgeInsets.fromLTRB(26, 0, 0, 0),
+            color: Colors.red,
+            
+            child: const Align(
+              alignment: Alignment.centerLeft,
+              child: Text( 'Nombre Proveedor:'))
+              ),
+          Container(
+            color: Colors.blue,
+            padding: const EdgeInsets.fromLTRB(26, 0, 0, 0),
+            child: const Align(
+              alignment: Alignment.centerLeft,
+              child: MyStatefulWidget())
+              
+              ),
+   
+          Container(
+            child: const Align(
+              alignment: Alignment.bottomLeft,
+              child: Text( 'Productos:')
+            ),
+            color: Colors.yellow,
+            padding: const EdgeInsets.fromLTRB(26, 40, 0, 0),
+            ),
+          
+        
+
+          ],
+        
+          ),
+        bottomSheet: const ButtonSaveCanceller(
+        title1: 'Siguiente',
+        title2: 'Cancelar',
       ),
     );
   }
+  }
+  
+  class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({Key? key}) : super(key: key);
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
 }
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  String dropdownValue = 'Soprole';
+
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButton<String>(
+      
+      value: dropdownValue,
+      icon: const Icon(Icons.arrow_downward),
+      elevation: 16,
+      
+      style: const TextStyle(color: Colors.black),
+      underline: Container(
+        height: 2,
+        color: Colors.deepPurpleAccent,
+      ),
+      onChanged: (String? newValue) {
+        setState(() {
+          dropdownValue = newValue!;
+        });
+      },
+      items: <String>['Soprole', 'Colun', 'Nestle']
+          .map<DropdownMenuItem<String>>((String value) {
+        return DropdownMenuItem<String>(
+          value: value,
+          child: Text(value),
+        );
+      }).toList(),
+    );
+  }
+}
+
+          
+      
+      
+  
--- lib/screens/PurchaseOrders/view_state_screen.dart
+++ lib/screens/PurchaseOrders/view_state_screen.dart
@@ -3,12 +3,168 @@ import 'package:flutter/material.dart';
 class ViewStateScreen extends StatelessWidget {
   const ViewStateScreen({Key? key}) : super(key: key);
 
+static const String titulo = 'Ver Estado Pedido';
+  
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('ViewStateScreen'),
-      ),
+
+    return Scaffold(    
+        
+      appBar: AppBar(
+        title: const Text(titulo),
+        leading: const Icon(Icons.menu_outlined),
+        elevation: 15.5, 
+        ),
+
+      body: Column(
+        children:  [
+          Container(
+            padding: const EdgeInsets.fromLTRB(26, 5, 0, 0),
+            color: Colors.red,
+            
+            child: const Align(
+              alignment: Alignment.centerLeft,
+              child: Text( 'Numero Orden de compra:'))
+              ),
+           Container(
+            padding: const EdgeInsets.fromLTRB(26, 5, 0, 0),
+            color: Colors.green,
+            child: const Align(
+              alignment: Alignment.centerLeft,
+              child: Text( '234234'))
+             
+           ),
+          Container(
+            color: Colors.blue,
+          
+            padding: const EdgeInsets.fromLTRB(26, 20, 0, 0),
+            child: const Align(
+              alignment: Alignment.centerLeft,
+              child: Text( 'Nombre Proveedor:')
+              )
+              
+              ),
+              Container(
+            padding: const EdgeInsets.fromLTRB(26, 5, 0, 0),
+            color: Colors.green,
+            child: const Align(
+              alignment: Alignment.centerLeft,
+              child: Text( 'Soprole'))
+             
+           ),
+   Row(
+              
+              children: [
+                
+                Container(
+                  alignment: Alignment.bottomLeft,
+                  padding: const EdgeInsets.fromLTRB(60, 40, 0, 0),
+                  color: Colors.red,
+                  child: const Icon(
+                    Icons.check,
+                    color: Colors.green),
+                ),
+                
+                Container(
+                  
+                child: const Align(
+                  alignment: Alignment.bottomLeft,
+                  child: Text( 'Pendiente de Envio')
+                ),
+                
+                color: Colors.yellow,
+                padding: const EdgeInsets.fromLTRB(20, 40, 0, 0),
+                ),
+              ],
+            ),
+          
+Row(
+              
+              children: [
+                
+                Container(
+                  alignment: Alignment.bottomLeft,
+                  padding: const EdgeInsets.fromLTRB(60, 40, 0, 0),
+                  color: Colors.red,
+                  child: const Icon(
+                    Icons.check,
+                    color: Colors.green),
+                ),
+                
+                Container(
+                  
+                child: const Align(
+                  alignment: Alignment.bottomLeft,
+                  child: Text( 'Enviado a Proveedor')
+                ),
+                
+                color: Colors.yellow,
+                padding: const EdgeInsets.fromLTRB(20, 40, 0, 0),
+                ),
+              ],
+            ),
+
+            Row(
+              
+              children: [
+                
+                Container(
+                  alignment: Alignment.bottomLeft,
+                  padding: const EdgeInsets.fromLTRB(60, 40, 0, 0),
+                  color: Colors.red,
+                  child: const Icon(
+                    Icons.check,
+                    color: Colors.green),
+                ),
+                
+                Container(
+                  
+                child: const Align(
+                  alignment: Alignment.bottomLeft,
+                  child: Text( 'Recibido en Bodega')
+                ),
+                
+                color: Colors.yellow,
+                padding: const EdgeInsets.fromLTRB(20, 40, 0, 0),
+                ),
+              ],
+            ),
+
+            
+            Row(
+              
+              children: [
+                
+                Container(
+                  alignment: Alignment.bottomLeft,
+                  padding: const EdgeInsets.fromLTRB(60, 40, 0, 0),
+                  color: Colors.red,
+                  child: const Icon(
+                    Icons.check,
+                    color: Colors.green),
+                ),
+                
+                Container(
+                  
+                child: const Align(
+                  alignment: Alignment.bottomLeft,
+                  child: Text( 'Pedido Finalizado')
+                ),
+                
+                color: Colors.yellow,
+                padding: const EdgeInsets.fromLTRB(20, 40, 0, 0),
+                ),
+              ],
+            ),
+          
+        
+
+          ],
+        
+          ),
+        
     );
   }
-}
+  }
+  
+ 
