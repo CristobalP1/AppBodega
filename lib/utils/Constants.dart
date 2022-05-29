7ecc1851d53feb6574cce7d87db33d6ccad22cfe
Jeremy <jeremyedu2014@gmail.com>
2022-05-29 02:52:11 -0400

    Enviado

--- lib/utils/Constants.dart
+++ lib/utils/Constants.dart
@@ -10,7 +10,7 @@ const String createOrder = "create_order";
 const String viewState = "view_state";
 const String createOrderFinish = "create_order_finish";
 //Providers Screen
-const String activeProvider = "active_provider";
+const String activeProvider = "active_providers_screen";
 const String inactiveProviders = "inactive_providers";
 const String createProvider = "create_provider";
 const String seeProductsVendors = "see_products_vendors";
