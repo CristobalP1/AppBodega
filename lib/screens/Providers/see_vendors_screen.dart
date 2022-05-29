4fc8076e94b4fe322b399f10182d5a1fd72eee32
Jeremy <jeremyedu2014@gmail.com>
2022-05-29 14:56:50 -0400

    Uploaded

--- lib/screens/Providers/see_vendors_screen.dart
+++ lib/screens/Providers/see_vendors_screen.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: unnecessary_import
+// ignore_for_file: unnecessary_import, avoid_unnecessary_containers
 
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/screens/screens.dart';
@@ -45,170 +45,175 @@ class SeeVendorsScreen extends StatelessWidget {
       ),
       backgroundColor: const Color.fromARGB(255, 255, 255, 255),
       // ignore: prefer_const_literals_to_create_immutables
-      body: Column(children: <Widget>[
-        const Padding(
-          padding: EdgeInsets.only(top: 15),
-        ),
-        const Align(
-          alignment: FractionalOffset(0.1, 0.1),
-          child: Text(
-            "Rut: 20.321.236-8",
-            style: TextStyle(
-                fontSize: 25,
-                color: Color.fromARGB(255, 0, 0, 0),
-                fontWeight: FontWeight.bold),
+      body: Container(
+        child: Column(children: <Widget>[
+          const Padding(
+            padding: EdgeInsets.only(top: 15),
           ),
-        ),
-        const Padding(
-          padding: EdgeInsets.only(top: 100),
-        ),
-        const Align(
-            alignment: FractionalOffset(0.01, 0.02),
-            child: Text("Datos Contacto:",
-                style: TextStyle(
-                    color: Colors.black,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 25))),
-        const Padding(
-          padding: EdgeInsets.only(top: 10),
-        ),
-        Container(
-          margin: const EdgeInsets.all(20),
-          child: Table(
-            border: TableBorder.all(
-              width: 2.5,
-              color: Colors.black,
+          const Align(
+            alignment: FractionalOffset(0.1, 0.1),
+            child: Text(
+              "Rut: 20.321.236-8",
+              style: TextStyle(
+                  fontSize: 25,
+                  color: Color.fromARGB(255, 0, 0, 0),
+                  fontWeight: FontWeight.bold),
             ),
-            children: const [
-              TableRow(
-                  decoration:
-                      BoxDecoration(color: Color.fromARGB(255, 159, 159, 159)),
-                  children: [
-                    Text(
-                      ' Correo',
-                      style: TextStyle(
-                          fontSize: 25,
-                          color: Color.fromARGB(255, 0, 0, 0),
-                          fontWeight: FontWeight.bold),
-                    ),
-                    Text(
-                      ' Nombre Vendedor',
-                      style: TextStyle(
-                          fontSize: 25,
-                          color: Color.fromARGB(255, 0, 0, 0),
-                          fontWeight: FontWeight.bold),
-                    )
-                  ]),
-              TableRow(
-                  decoration:
-                      BoxDecoration(color: Color.fromARGB(255, 213, 213, 213)),
-                  children: [
-                    Text(
-                      ' colun_juan_retamales@colunchile.cl',
-                      style: TextStyle(
-                          fontSize: 20, color: Color.fromARGB(255, 0, 0, 0)),
-                    ),
-                    Text(
-                      ' Juan Retamales',
-                      style: TextStyle(
-                          fontSize: 20, color: Color.fromARGB(255, 0, 0, 0)),
-                    )
-                  ]),
-              TableRow(
-                  decoration:
-                      BoxDecoration(color: Color.fromARGB(255, 213, 213, 213)),
-                  children: [
-                    Text(
-                      ' colun_mario_gutierrez@colunchile.cl',
-                      style: TextStyle(
-                          fontSize: 20, color: Color.fromARGB(255, 0, 0, 0)),
-                    ),
-                    Text(
-                      ' Mario Gutierrez',
-                      style: TextStyle(
-                          fontSize: 20, color: Color.fromARGB(255, 0, 0, 0)),
-                    )
-                  ])
-            ],
           ),
-        ),
-        const Padding(
-          padding: EdgeInsets.only(top: 50),
-        ),
-        FloatingActionButton.extended(
-          onPressed: () {
-            Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (context) => const SeeProductsVendorsScreen()));
-          },
-          backgroundColor: const Color.fromARGB(255, 0, 255, 140),
-          label: const Text(
-            'Ver Productos',
-            style: TextStyle(color: Color.fromARGB(255, 0, 0, 0)),
+          const Padding(
+            padding: EdgeInsets.only(top: 100),
           ),
-          icon: const Icon(
-            Icons.visibility,
-            color: Color.fromARGB(255, 0, 0, 0),
+          const Align(
+              alignment: FractionalOffset(0.01, 0.02),
+              child: Text("Datos Contacto:",
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 25))),
+          const Padding(
+            padding: EdgeInsets.only(top: 10),
           ),
-        ),
-        const Padding(
-          padding: EdgeInsets.only(top: 10),
-        ),
-        FloatingActionButton.extended(
-          onPressed: () {
-            showDialog(
-                context: context,
-                builder: (context) => AlertDialog(
-                      actions: [
-                        TextButton(
-                          onPressed: () {
-                            Navigator.of(context).pop();
-                            showDialog(
-                                context: context,
-                                builder: (context) => AlertDialog(
-                                      actions: [
-                                        TextButton(
-                                          onPressed: () {
-                                            Navigator.of(context).pop();
-                                          },
-                                          child: const Text('Aceptar'),
-                                        ),
-                                      ],
-                                      title: const Text('Alerta Desactivación'),
-                                      contentPadding:
-                                          const EdgeInsets.all(20.0),
-                                      content: const Text(
-                                        'Proveedor Desactivado',
-                                      ),
-                                    ));
-                          },
-                          child: const Text('Aceptar'),
-                        ),
-                        TextButton(
-                          onPressed: () {
-                            Navigator.of(context).pop();
-                          },
-                          child: const Text('Cancelar'),
-                        ),
-                      ],
-                      title: const Text('Alerta Desactivación'),
-                      contentPadding: const EdgeInsets.all(20.0),
-                      content: const Text(
-                          '¿Estas seguro que quieres desactivar a este proveedor?'),
-                    ));
-          },
-          backgroundColor: const Color.fromARGB(255, 255, 98, 98),
-          label: const Text(
-            'Desactivar',
-            style: TextStyle(color: Color.fromARGB(255, 0, 0, 0)),
+          Container(
+            margin: const EdgeInsets.all(20),
+            child: Table(
+              border: TableBorder.all(
+                width: 2.5,
+                color: Colors.black,
+              ),
+              children: const [
+                TableRow(
+                    decoration: BoxDecoration(
+                        color: Color.fromARGB(255, 159, 159, 159)),
+                    children: [
+                      Text(
+                        ' Correo',
+                        style: TextStyle(
+                            fontSize: 25,
+                            color: Color.fromARGB(255, 0, 0, 0),
+                            fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        ' Nombre Vendedor',
+                        style: TextStyle(
+                            fontSize: 25,
+                            color: Color.fromARGB(255, 0, 0, 0),
+                            fontWeight: FontWeight.bold),
+                      )
+                    ]),
+                TableRow(
+                    decoration: BoxDecoration(
+                        color: Color.fromARGB(255, 213, 213, 213)),
+                    children: [
+                      Text(
+                        ' colun_juan_retamales@colunchile.cl',
+                        style: TextStyle(
+                            fontSize: 20, color: Color.fromARGB(255, 0, 0, 0)),
+                      ),
+                      Text(
+                        ' Juan Retamales',
+                        style: TextStyle(
+                            fontSize: 20, color: Color.fromARGB(255, 0, 0, 0)),
+                      )
+                    ]),
+                TableRow(
+                    decoration: BoxDecoration(
+                        color: Color.fromARGB(255, 213, 213, 213)),
+                    children: [
+                      Text(
+                        ' colun_mario_gutierrez@colunchile.cl',
+                        style: TextStyle(
+                            fontSize: 20, color: Color.fromARGB(255, 0, 0, 0)),
+                      ),
+                      Text(
+                        ' Mario Gutierrez',
+                        style: TextStyle(
+                            fontSize: 20, color: Color.fromARGB(255, 0, 0, 0)),
+                      )
+                    ])
+              ],
+            ),
           ),
-          icon: const Icon(
-            Icons.visibility_off,
-            color: Color.fromARGB(255, 0, 0, 0),
+          const Padding(
+            padding: EdgeInsets.only(top: 50),
           ),
-        ),
-      ]),
+          FloatingActionButton.extended(
+            onPressed: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const SeeProductsVendorsScreen()));
+            },
+            backgroundColor: const Color.fromARGB(255, 0, 255, 140),
+            label: const Text(
+              'Ver Productos',
+              style: TextStyle(color: Color.fromARGB(255, 0, 0, 0)),
+            ),
+            icon: const Icon(
+              Icons.visibility,
+              color: Color.fromARGB(255, 0, 0, 0),
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.only(top: 10),
+          ),
+          Container(
+            child: FloatingActionButton.extended(
+              onPressed: () {
+                showDialog(
+                    context: context,
+                    builder: (context) => AlertDialog(
+                          actions: [
+                            TextButton(
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                                showDialog(
+                                    context: context,
+                                    builder: (context) => AlertDialog(
+                                          actions: [
+                                            TextButton(
+                                              onPressed: () {
+                                                Navigator.of(context).pop();
+                                              },
+                                              child: const Text('Aceptar'),
+                                            ),
+                                          ],
+                                          title: const Text(
+                                              'Alerta Desactivación'),
+                                          contentPadding:
+                                              const EdgeInsets.all(20.0),
+                                          content: const Text(
+                                            'Proveedor Desactivado',
+                                          ),
+                                        ));
+                              },
+                              child: const Text('Aceptar'),
+                            ),
+                            TextButton(
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                              child: const Text('Cancelar'),
+                            ),
+                          ],
+                          title: const Text('Alerta Desactivación'),
+                          contentPadding: const EdgeInsets.all(20.0),
+                          content: const Text(
+                              '¿Estas seguro que quieres desactivar a este proveedor?'),
+                        ));
+              },
+              backgroundColor: const Color.fromARGB(255, 255, 98, 98),
+              label: const Text(
+                'Desactivar',
+                style: TextStyle(color: Color.fromARGB(255, 0, 0, 0)),
+              ),
+              icon: const Icon(
+                Icons.visibility_off,
+                color: Color.fromARGB(255, 0, 0, 0),
+              ),
+            ),
+          ),
+        ]),
+      ),
     );
   }
 }
