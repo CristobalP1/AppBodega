21631c69cf140996821b08a986de517d3964d9af
piroman <frab369gmail.com>
2022-06-17 16:52:38 -0400

    escreen update

--- lib/screens/Products/edit_product_screen.dart
+++ lib/screens/Products/edit_product_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/widget/Inputs/input_form.dart';
+import 'package:flutter_application_1/widget/Drawer/Drawer.dart';
 import 'package:flutter_application_1/widget/Buttons/button_save_canceller.dart';
 
 class EditProductScreen extends StatelessWidget {
@@ -10,8 +11,6 @@ class EditProductScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Productos'),
-        backgroundColor: Colors.pinkAccent,
-        leading: const Icon(Icons.menu_outlined),
         actions: [
           IconButton(
             icon: const Icon(Icons.attach_file_rounded),
@@ -23,91 +22,79 @@ class EditProductScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                const Text(
-                  'Editar producto',
-                  style: TextStyle(fontSize: 20),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    const Icon(
-                      Icons.create_sharp,
-                      color: Colors.pinkAccent,
-                      size: 30.0,
-                    ),
-                    ElevatedButton(
-                      onPressed: () {},
-                      child: const Text('CONTAINED BUTTON'),
-                    )
-                  ],
-                ),
-              ],
+          children: const [
+            Text(
+              '',
+              style: TextStyle(fontSize: 15),
             ),
-            const InputForm(
-              labelText: "Leche",
+            SizedBox(
+              height: 1,
+            ),
+            Text(
+              'Editar producto',
+              style: TextStyle(fontSize: 25),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            InputForm(
+              labelText: "Nombre: Leche",
               hintText: "leche",
             ),
-            const SizedBox(
+            SizedBox(
               height: 10,
             ),
-            const InputForm(
-              labelText: "Lacteos",
+            InputForm(
+              labelText: "Categoria: Lacteos",
               hintText: "Lacteos",
             ),
-            const SizedBox(
+            SizedBox(
               height: 10,
             ),
-            const InputForm(
-              labelText: "23231",
+            InputForm(
+              labelText: "SKU: 23231",
               hintText: "23231",
             ),
-            const SizedBox(
+            SizedBox(
               height: 10,
             ),
-            const InputForm(
-              labelText: "780231223",
+            InputForm(
+              labelText: "Codigo de barras: 780231223",
               hintText: "780231223",
             ),
-            const SizedBox(
+            SizedBox(
               height: 10,
             ),
-            const InputForm(
-              labelText: "23",
+            InputForm(
+              labelText: "Stock: 23",
               hintText: "23",
             ),
-            const SizedBox(
+            SizedBox(
               height: 10,
             ),
-            const InputForm(
-              labelText: "3",
+            InputForm(
+              labelText: "Stock critico: 3",
               hintText: "3",
             ),
-            const SizedBox(
+            SizedBox(
               height: 10,
             ),
-            const InputForm(
-              labelText: "3990",
+            InputForm(
+              labelText: "Precio: 3990",
               hintText: "3990",
             ),
-            const SizedBox(
+            SizedBox(
               height: 25,
             ),
           ],
         ),
       ),
       bottomSheet: const ButtonSaveCanceller(
-        next: 'Cancelar',
-        back: 'Guardar cambios',
+        next: 'Guardar cambios',
+        back: 'Cancelar',
       ),
+      drawer: const ComplexDrawer(),
+      drawerScrimColor: Colors.transparent,
     );
   }
 }
