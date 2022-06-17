c8061ec87ce9713548098f58533a648958f85358
cristobal-1 <cristobal1@gmail.com>
2022-06-17 16:57:03 -0400

    adding history orders pages

---
+++ lib/screens/PurchaseOrders/details_orders.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class DetailsOrdersScreen extends StatelessWidget {
+  const DetailsOrdersScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('DetailsOrdersScreen'),
+      ),
+    );
+  }
+}
---
+++ lib/screens/PurchaseOrders/history_orders.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class HistoryOrdersScreen extends StatelessWidget {
+  const HistoryOrdersScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('HistoryOrdersScreen'),
+      ),
+    );
+  }
+}
