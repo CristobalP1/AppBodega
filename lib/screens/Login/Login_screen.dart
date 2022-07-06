24dffee43328b16138d3867a1d7bc531684b707f
cristobal-1 <cristobal1@gmail.com>
2022-07-05 20:37:59 -0400

    change loging screen

--- lib/screens/Login/Login_screen.dart
+++ lib/screens/Login/Login_screen.dart
@@ -1,14 +1,13 @@
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatelessWidget {
-
   const LoginScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
       body: Center(
-         child: Text('LoginScreen'),
+        child: Text('LoginScreen'),
       ),
     );
   }
