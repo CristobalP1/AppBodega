aaae086948099e4e14a3f2b8e5e8d36e9722485d
cristobal-1 <cristobal1@gmail.com>
2022-07-05 20:38:52 -0400

    addin change login screen 2

--- lib/screens/Login/Login_screen.dart
+++ lib/screens/Login/Login_screen.dart
@@ -7,7 +7,7 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Scaffold(
       body: Center(
-        child: Text('LoginScreen'),
+        child: Text('LoginScreen2'),
       ),
     );
   }
