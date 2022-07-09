770bde7b6ea66e0bc381a50f8a1bee0067799eb1
piroman <frab369gmail.com>
2022-07-08 23:46:00 -0400

    Create Category-locked

--- lib/screens/Category/category_locked_screen.dart
+++ lib/screens/Category/category_locked_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/screens/Category/create_category_screen.dart';
+import 'package:provider/provider.dart';
+import '../../Providers/list_category_provider.dart';
 
 import '../../theme/app_theme.dart';
 
@@ -7,6 +10,7 @@ class CategoryLockedScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final listado = Provider.of<ListCategoryProvider>(context);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Productos'),
@@ -24,8 +28,34 @@ class CategoryLockedScreen extends StatelessWidget {
           )
         ],
       ),
-      body: Center(
-        child: Text('CategoryLockedScreen'),
+      body: Container(
+        margin: const EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            const Text(
+              'Lista de categorias bloqueadas',
+              style: TextStyle(fontSize: 20),
+            ),
+            Row(
+              children: <Widget>[],
+            ),
+            Flexible(
+              child: ListView.builder(
+                itemCount: listado.ListCategorias.length,
+                itemBuilder: (context, index) => ListTile(
+                  leading: const Icon(Icons.archive_outlined),
+                  title: Text(listado.ListCategorias[index].nombreCategoria),
+                  trailing: const Icon(Icons.content_paste_off),
+                  onTap: () {
+                    const Icon(Icons.archive_outlined);
+                  },
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
